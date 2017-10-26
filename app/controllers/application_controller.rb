65ad5698326e5c7abd2aa7005a63ff06d5cd7cd3
alejovelez10 <alejovelez10@gmail.com>
2017-10-26 00:46:31 -0500

    comenzando inicio

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+layout :layout_for_selection
+protected
+  def layout_for_selection
+    if controller_name == 'sessions'  || controller_name == 'passwords' 
+      'application'
+    elsif controller_name == 'registrations'
+
+        if action_name == "new" || action_name == "create"
+            'application'
+            else
+            	'admin'
+        end
+    elsif (controller_name == 'home' and (action_name == "inicio")) 
+      
+   
+    'home' 
+
+
+      
+    else
+      'application'
+  
+    end
+  end
+
 end
