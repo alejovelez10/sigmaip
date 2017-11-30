11bd2791e7c75a3c3ec1a9d1789cfee43afdc32d
alejovelez10 <alejovelez10@gmail.com>
2017-11-29 23:06:46 -0500

    organizando

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,19 @@ if fecha != nil
 #dias[ds] + ", " +
 end 
 end
+
+def get_datew(fecha)
+   
+if fecha != nil
+    ds = fecha.strftime("%w") #Dia de la semana
+    y = fecha.strftime("%Y") #Año
+    dm = fecha.strftime("%d") #Dia del mes
+    m = fecha.strftime("%m") # Mes del Año
+    meses = {"01" => "Enero", "02" => "Febrero","03"=>"Marzo","04" => "Abril", "05" => "Mayo","06"=> "Junio" ,"07"=> "Julio", "08" => "Agosto", "09"=> "Septiembre" ,"10"=> "Octubre","11" => "Noviembre" ,"12" => "Diciembre" }
+    dias = {"1" => "Domingo", "2" => "Lunes","3"=>"Martes","4" => "Miercoles", "5" => "Jueves","6"=> "Viernes" ,"0" =>"Sabado"}
+
+    return  meses[m] + " " + dm + ", " + y  
+#dias[ds] + ", " +
+end 
+end
 end
